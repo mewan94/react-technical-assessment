b77fc5d604ecbc50f8d3ad1696aa8df3b64c1b77
Mewan Suriyaarachchi <kalharamewan@gmail.com>
2022-04-30 12:41:23 +0530

    code cleanup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import userFormReducer from '../features/userForm/userFormSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     userForm: userFormReducer,
   },
 });
